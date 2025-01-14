d87f3b061c950de7ec8b6eca5562af57dae322d7
Yoon-Beom <dbsqja2100@naver.com>
2025-01-14 13:48:00 +0900

    Change Ganache -> MetaMask

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,8 @@ const App = () => {
   const [contract, setContract] = useState(null);
   const [hotels, setHotels] = useState([]);
   const [loading, setLoading] = useState(true);
-  // 새로운 호텔 정보를 위한 상태 추가
+  const [web3, setWeb3] = useState(null);
+  // 새로운 호텔 정보를 위한 상태
   const [newHotelName, setNewHotelName] = useState('');
   const [newHotelIpfsHash, setNewHotelIpfsHash] = useState('');
   const [newRoomNumber, setNewRoomNumber] = useState('');
@@ -18,33 +19,65 @@ const App = () => {
 
   useEffect(() => {
     const init = async () => {
-      // Web3 인스턴스 생성
-      const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+      // 메타마스크가 설치되어 있는지 확인
+      if (typeof window.ethereum !== 'undefined') {
+        try {
+          // 사용자에게 계정 연결 권한 요청
+          await window.ethereum.request({ method: 'eth_requestAccounts' });
 
-      // 계정 가져오기
-      const accounts = await web3.eth.getAccounts();
-      setAccount(accounts[0]);
+          // Web3 인스턴스 생성
+          const web3Instance = new Web3(window.ethereum);
+          setWeb3(web3Instance);
 
-      // 잔액 가져오기
-      const balanceWei = await web3.eth.getBalance(accounts[0]);
-      const balanceEth = web3.utils.fromWei(balanceWei, 'ether');
-      setBalance(balanceEth);
+          // 연결된 계정 가져오기
+          const accounts = await web3Instance.eth.getAccounts();
+          setAccount(accounts[0]);
 
-      // 네트워크 ID 가져오기
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = HotelBookingContract.networks[networkId];
+          // 잔액 가져오기
+          const balanceWei = await web3Instance.eth.getBalance(accounts[0]);
+          const balanceEth = web3Instance.utils.fromWei(balanceWei, 'ether');
+          setBalance(balanceEth);
 
-      // 컨트랙트 인스턴스 생성
-      const instance = new web3.eth.Contract(
-        HotelBookingContract.abi,
-        deployedNetwork && deployedNetwork.address
-      );
+          // 네트워크 ID 가져오기
+          const networkId = await web3Instance.eth.net.getId();
+          const deployedNetwork = HotelBookingContract.networks[networkId];
 
-      setContract(instance);
-      await loadHotels(instance);
+          // 컨트랙트 인스턴스 생성
+          const instance = new web3Instance.eth.Contract(
+            HotelBookingContract.abi,
+            deployedNetwork && deployedNetwork.address
+          );
+
+          setContract(instance);
+          await loadHotels(instance);
+        } catch (error) {
+          console.error("사용자가 계정 접근을 거부했거나 오류가 발생했습니다:", error);
+        }
+      } else {
+        console.log('메타마스크를 설치해주세요!');
+      }
     };
     init();
   }, []);
+
+  // 계정 변경 감지
+  useEffect(() => {
+    if (window.ethereum) {
+      window.ethereum.on('accountsChanged', (accounts) => {
+        setAccount(accounts[0]);
+        updateBalance(accounts[0]);
+      });
+    }
+  }, []);
+
+  // 잔액 업데이트 함수
+  const updateBalance = async (address) => {
+    if (web3) {
+      const balanceWei = await web3.eth.getBalance(address);
+      const balanceEth = web3.utils.fromWei(balanceWei, 'ether');
+      setBalance(balanceEth);
+    }
+  };
 
   const loadHotels = async (instance) => {
     const hotelCount = await instance.methods.hotelCount().call();
@@ -86,33 +119,39 @@ const App = () => {
     }
   };
 
-    // 새로운 방을 추가하는 함수
-    const addRoom = async () => {
-      if (!contract || !selectedHotelId) return;
-      try {
-        await contract.methods.addRoom(
-          selectedHotelId,
-          newRoomNumber,
-          Web3.utils.toWei(newRoomPrice, 'ether'),
-          newRoomIpfsHash
-        ).send({ 
-          from: account,
-          gas: 500000 // 가스 한도 설정
-        });
-        await loadHotels(contract);
-        setNewRoomNumber('');
-        setNewRoomPrice('');
-        setNewRoomIpfsHash('');
-      } catch (error) {
-        console.error("Error adding room:", error);
-      }
-    };
+  // 새로운 방을 추가하는 함수
+  const addRoom = async () => {
+    if (!contract || !selectedHotelId) return;
+    try {
+      await contract.methods.addRoom(
+        selectedHotelId,
+        newRoomNumber,
+        Web3.utils.toWei(newRoomPrice, 'ether'),
+        newRoomIpfsHash
+      ).send({
+        from: account,
+        gas: 500000 // 가스 한도 설정
+      });
+      await loadHotels(contract);
+      setNewRoomNumber('');
+      setNewRoomPrice('');
+      setNewRoomIpfsHash('');
+    } catch (error) {
+      console.error("Error adding room:", error);
+    }
+  };
 
   return (
     <div>
       <h1>호텔 예약 시스템</h1>
-      <p>계정 주소: {account}</p>
-      <p>잔액: {balance} ETH</p>
+      {account ? (
+        <>
+          <p>연결된 계정 주소: {account}</p>
+          <p>잔액: {balance} ETH</p>
+        </>
+      ) : (
+        <p>메타마스크에 연결되지 않았습니다. 연결해주세요.</p>
+      )}
 
       {/* 호텔 등록 폼 */}
       <h2>새 호텔 등록</h2>
